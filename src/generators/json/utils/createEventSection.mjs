d711fee5348c52327eb793b0266b18efef7285a8
flakey5 <73616808+flakey5@users.noreply.github.com>
2025-10-06 13:15:47 -0700

    feat: json, json-all generators
    Closes #214
    
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

---
+++ src/generators/json/utils/createEventSection.mjs
@@ -0,0 +1,203 @@
+// @ts-check
+import { GeneratorError } from '../../../utils/generator-error.mjs';
+import { EVENT_TYPE_DESCRIPTION_EXTRACTOR } from '../constants.mjs';
+import { findParentSection } from './findParentSection.mjs';
+import { stringifyNode } from './stringifyNode.mjs';
+
+/**
+ * @typedef {import('../../legacy-json/types.d.ts').HierarchizedEntry} HierarchizedEntry
+ */
+
+export const createEventSectionBuilder = () => {
+  /**
+   * Parse the parameters for the event's callback method
+   * @param {HierarchizedEntry} entry The AST entry
+   * @param {import('../generated.d.ts').Event} section The event section
+   */
+  const parseParameters = (entry, section) => {
+    const [, ...nodes] = entry.content.children;
+    const listNode = nodes[0];
+
+    // If an event has type info it should be the first thing in its child
+    // elements
+    if (!listNode || listNode.type !== 'list') {
+      // No parameters
+      return;
+    }
+
+    section.parameters ??= [];
+
+    for (const node of listNode.children) {
+      let parameterAst = node.children[0];
+      if (parameterAst.type !== 'paragraph') {
+        throw new TypeError(
+          `expected ast type 'paragraph', got ${parameterAst.type}`
+        );
+      }
+
+      /**
+       * @type {import('../generated.d.ts').MethodParameter}
+       */
+      const parameter = {};
+
+      // This list node can be in three different formats:
+      // 1. `paramName` <type> [description]
+      // 2. Type: <type> [description]
+      // 3. <type> [description]
+      switch (parameterAst.children[0]?.type) {
+        case 'inlineCode': {
+          // First format
+          if (parameterAst.children.length < 2) {
+            throw new GeneratorError(
+              `expected min 2 children, got ${parameterAst.children.length}`,
+              { entry }
+            );
+          }
+
+          const [name, delimiter, ...rest] = parameterAst.children;
+          parameter['@name'] = name.value;
+
+          if (delimiter.type !== 'text') {
+            throw new GeneratorError(
+              `expected delimiter child type in list node to be 'text', got ${delimiter.type} (@name=${parameter['@name']})`,
+              { entry }
+            );
+          }
+
+          // todo why does delimiter.value === ' ' = false even when that's the case ????????
+          if (rest[0]?.type === 'link') {
+            // Type _should_ be a link with maybe a description following
+            const [type, ...descriptionNodes] = rest;
+
+            // TODO type can be | thing here as well :)
+            parameter['@type'] = type.children[0].value;
+
+            let description = '';
+            descriptionNodes.forEach(
+              node => (description += stringifyNode(node))
+            );
+
+            if (description !== '') {
+              parameter.description = description;
+            }
+          } else {
+            // Type isn't a link and we get the joy of extracting it
+            const value = EVENT_TYPE_DESCRIPTION_EXTRACTOR.exec(
+              delimiter.value
+            );
+            if (value === null) {
+              throw new GeneratorError(
+                `failed extracting type & description from '${delimiter.value}'`,
+                { entry }
+              );
+            }
+
+            parameter['@type'] = value[1].trim();
+            parameter.description = value[2].trim();
+          }
+
+          break;
+        }
+        case 'text': {
+          // Second format, pop the `Type: ` literal and then fallthrough to
+          // parsing it like the third format
+
+          if (parameterAst.children[0].value !== 'Type: ') {
+            // Not what we want
+            continue;
+          }
+
+          parameterAst = {
+            ...parameterAst,
+            children: structuredClone(parameterAst.children),
+          };
+          parameterAst.children.shift();
+
+          // Fallthrough on purpose
+        }
+        case 'link': {
+          // Third format
+          let type = '';
+
+          let i = 0;
+          let moreTypes = false;
+          do {
+            const node = parameterAst.children[i];
+            if (node.type !== 'link') {
+              throw new GeneratorError(
+                `expected type 'link', got ${node.type}`,
+                { entry }
+              );
+            }
+
+            if (node.children[0].type !== 'inlineCode') {
+              throw new GeneratorError(
+                `expected type 'inlineCode', got ${node.children[0].type}`,
+                { entry }
+              );
+            }
+
+            type += node.children[0].value;
+
+            // Check if this link is followed up by a `|`
+            const nextNode = parameterAst.children[i + 1];
+            moreTypes =
+              nextNode &&
+              nextNode.type === 'text' &&
+              nextNode.value.trim() === '|';
+            if (moreTypes) {
+              type += ' | ';
+            }
+            i += 2;
+          } while (moreTypes);
+
+          parameter['@name'] = 'value';
+          parameter['@type'] = type;
+
+          let description = '';
+          for (; i < parameterAst.children.length; i++) {
+            description += stringifyNode(parameterAst.children[i]);
+          }
+
+          if (description !== '') {
+            parameter.description = description;
+          }
+
+          break;
+        }
+        default: {
+          throw new TypeError(
+            `unexpected list node type: ${parameterAst.children[0].type}`
+          );
+        }
+      }
+
+      parameter['@name'] = parameter['@name']
+        .replaceAll("'", '')
+        .replaceAll('<', '')
+        .replaceAll('>', '');
+
+      if (parameter.description) {
+        parameter.description = parameter.description.trim();
+      }
+
+      section.parameters.push(parameter);
+    }
+  };
+
+  /**
+   * Adds the properties expected in an event section to an object.
+   * @param {HierarchizedEntry} entry The AST entry
+   * @param {import('../generated.d.ts').Event} section The event section
+   */
+  return (entry, section) => {
+    parseParameters(entry, section);
+
+    const parent = findParentSection(section, ['class', 'module']);
+
+    // Add this section to the parent if it exists
+    if (parent) {
+      parent.events.push(section);
+    }
+  };
+};
---
+++ src/generators/json/utils/createMethodSection.mjs
@@ -0,0 +1,155 @@
+// @ts-check
+'use strict';
+
+import { GeneratorError } from '../../../utils/generator-error.mjs';
+import { findParentSection } from './findParentSection.mjs';
+
+/**
+ * @typedef {import('../../legacy-json/types.d.ts').HierarchizedEntry} HierarchizedEntry
+ *
+ * @typedef {{ type: 'parameter' } | { type: 'returnValue' }} ParameterListNode
+ */
+
+export const createMethodSectionBuilder = () => {
+  /**
+   * Handles each node in a parameter list
+   * @param {import('mdast').ListItem} param0
+   * @returns {import('../generated.d.ts').MethodParameter}
+   */
+  const parseParameterListNode = ({ children }) => {
+    /**
+     * `paragraph` will be a parameter's type declaration (ex/ "`asd` {string} Description of asd")
+     * or the method's return value (ex/ "Returns: {integer}")
+     *
+     * `list` only occurs when the parameter is an object that has documented properties
+     */
+    const [paragraph, list] = children;
+
+    // Safety checks
+    if (paragraph.type !== 'paragraph') {
+      throw new GeneratorError(
+        `expected first node in parameter list node to be a paragraph`
+      );
+    }
+
+    if (list && list.type !== 'list') {
+      throw new GeneratorError(
+        `expected second node in parameter list item to be a list`
+      );
+    }
+
+    const [parameterNameNode] = paragraph.children;
+
+    switch (parameterNameNode.type) {
+      case 'inlineCode': {
+        // node is something like: "`asd` {string} Description of asd"
+        const [
+          ,
+          blankSpaceNode,
+          parameterTypeNode,
+          ...parameterDescriptionNodes
+        ] = paragraph.children;
+
+        // if (blankSpaceNode.type !== 'text' || blankSpaceNode.value !== ' ') {
+        //   console.log('asdasd', blankSpaceNode, blankSpaceNode.type, `"${blankSpaceNode.value}"`);
+        //   throw new TypeError(
+        //     `expected blank space between parameter name and type`
+        //   );
+        // }
+
+        if (parameterTypeNode.type !== 'link') {
+          // console.log('asd', JSON.stringify(paragraph.children, null, 2));
+          throw new TypeError(`expected parameter type to be a link`);
+        }
+
+        // console.log(parameterTypeNode);
+
+        break;
+      }
+      case 'text': {
+        // node is something like: "Returns: {integer}""
+        break;
+      }
+    }
+
+    return {
+      '@name': 'asd',
+    };
+  };
+
+  /**
+   * Parses the parameters that the method accepts
+   * @param {HierarchizedEntry} entry The AST entry
+   * @returns {Record<string, import('../generated.d.ts').MethodParameter> | undefined}
+   */
+  const parseParameters = entry => {
+    const [, ...nodes] = entry.content.children;
+
+    // The first list that exists in a doc entry should be the method's
+    // parameter list.
+    const listNode = nodes.find(node => node.type === 'list');
+
+    if (!listNode) {
+      // Method doesn't take in any parameters
+      return undefined;
+    }
+
+    /**
+     * @type {Record<string, import('../generated.d.ts').MethodParameter>}
+     */
+    const parameters = {};
+
+    listNode.children.forEach(listItem => {
+      const parameter = parseParameterListNode(listItem);
+
+      parameter[parameter['@name']] = parameter;
+    });
+
+    return parameters;
+  };
+
+  /**
+   * Parses the signatures that the method may have and adds them to the
+   * section.
+   * @param {HierarchizedEntry} entry The AST entry
+   * @param {import('../generated.d.ts').Method} section The method section
+   */
+  const parseSignatures = (entry, section) => {
+    section.signatures = [];
+
+    // Parse all the parameters and store them in a name:section map
+    const parameters = parseParameters(entry, section);
+
+    // Parse the value of entry.heading.data.text to get the order of parameters and which are optional
+    // console.log(entry.heading.data.text);
+  };
+
+  /**
+   * Adds the properties expected in a method section to an object.
+   * @param {HierarchizedEntry} entry The AST entry
+   * @param {import('../generated.d.ts').Method} section The method section
+   */
+  return (entry, section) => {
+    parseSignatures(entry, section);
+
+    const parent = findParentSection(section, ['class', 'module']);
+
+    // Add this section to the parent if it exists
+    if (parent) {
+      // Put static methods in `staticMethods` property and non-static methods
+      // in the `methods` property
+      const property = entry.heading.data.text.startsWith('Static method:')
+        ? 'staticMethods'
+        : 'methods';
+
+      if (!Array.isArray(parent[property])) {
+        throw new GeneratorError(
+          `expected parent[${property}] to be an array, got type ${typeof parent[property]} instead (parent type=${parent.type})`,
+          { entry }
+        );
+      }
+
+      parent[property].push(section);
+    }
+  };
+};
