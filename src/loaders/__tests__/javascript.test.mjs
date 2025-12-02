d33209bd5d6aa30dc7628cb70ccb7285332a6598
Guilherme Araújo <arauujogui@gmail.com>
2025-12-02 19:30:42 -0300

    test(loaders): create unit tests

---
+++ src/loaders/__tests__/javascript.test.mjs
@@ -0,0 +1,45 @@
+// @ts-check
+
+'use strict';
+
+import assert from 'node:assert/strict';
+import { describe, it, mock } from 'node:test';
+
+import { VFile } from 'vfile';
+
+const fakeFiles = [
+  '/fake/path/file1.js',
+  '/fake/path/file2.js',
+  '/fake/path/not-a-js-file.txt',
+];
+
+const fakeContent = 'const a = 1;';
+
+mock.module('node:fs', {
+  namedExports: {
+    globSync: () => fakeFiles,
+  },
+});
+
+mock.module('node:fs/promises', {
+  namedExports: {
+    readFile: () => Promise.resolve(fakeContent),
+  },
+});
+
+const createLoader = (await import('../javascript.mjs')).default;
+
+describe('javascript-loader', () => {
+  it('should load javascript files into VFiles', async () => {
+    const loader = createLoader();
+    const vfiles = await loader.loadFiles('*.js');
+
+    assert.strictEqual(vfiles.length, 2); // Ignored the .txt file
+
+    assert.ok(vfiles[0] instanceof VFile);
+    assert.strictEqual(vfiles[0].path, fakeFiles[0]);
+    assert.strictEqual(vfiles[0].value, fakeContent);
+    assert.strictEqual(vfiles[1].path, fakeFiles[1]);
+    assert.strictEqual(vfiles[1].value, fakeContent);
+  });
+});
---
+++ src/loaders/__tests__/markdown.test.mjs
@@ -0,0 +1,61 @@
+// @ts-check
+
+'use strict';
+
+import assert from 'node:assert/strict';
+import { describe, it, mock } from 'node:test';
+import { isDeepStrictEqual } from 'node:util';
+
+import { VFile } from 'vfile';
+
+const fakeContent = '# Hello';
+mock.module('node:fs/promises', {
+  namedExports: {
+    readFile: () => Promise.resolve(fakeContent),
+  },
+});
+
+mock.module('node:fs', {
+  namedExports: {
+    globSync: (/** @type {string[]} */ path) => {
+      if (isDeepStrictEqual(path, ['ignore.md'])) {
+        return ['/fake/path/file2.md'];
+      }
+      return [
+        '/fake/path/file1.md',
+        '/fake/path/file2.md',
+        '/fake/path/not-a-md-file.txt',
+      ];
+    },
+  },
+});
+
+const { default: createLoader } = await import('../markdown.mjs');
+
+describe('markdown-loader', () => {
+  it('should load markdown files into VFiles', async () => {
+    const fakeFiles = [
+      '/fake/path/file1.md',
+      '/fake/path/file2.md',
+      '/fake/path/not-a-md-file.txt',
+    ];
+
+    const loader = createLoader();
+    const vfiles = await loader.loadFiles(['*.md']);
+
+    assert.strictEqual(vfiles.length, 2);
+    assert.ok(vfiles[0] instanceof VFile);
+    assert.strictEqual(vfiles[0].path, fakeFiles[0]);
+    assert.strictEqual(vfiles[0].value, fakeContent);
+    assert.strictEqual(vfiles[1].path, fakeFiles[1]);
+    assert.strictEqual(vfiles[1].value, fakeContent);
+  });
+
+  it('should ignore specified files', async () => {
+    const loader = createLoader();
+    const vfiles = await loader.loadFiles(['*.md'], ['ignore.md']);
+
+    assert.strictEqual(vfiles.length, 1);
+    assert.strictEqual(vfiles[0].path, '/fake/path/file1.md');
+  });
+});
