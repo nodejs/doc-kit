db78639a9965af132cec1c302d55cbd8850a385e
Aviv Keller <me@aviv.sh>
2025-12-16 11:03:58 -0500

    feat(comparisons): SHA unifying, `legacy-json` comparison

---
+++ scripts/compare-builds/legacy-json.mjs
@@ -0,0 +1,31 @@
+import assert from 'node:assert';
+import { readdir, readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+import { BASE, HEAD } from './utils.mjs';
+
+const files = await readdir(BASE);
+
+const results = await Promise.all(
+  files.map(async file => {
+    const basePath = join(BASE, file);
+    const headPath = join(HEAD, file);
+
+    const baseContent = JSON.parse(await readFile(basePath, 'utf-8'));
+    const headContent = JSON.parse(await readFile(headPath, 'utf-8'));
+
+    try {
+      assert.deepStrictEqual(baseContent, headContent);
+      return null;
+    } catch ({ message }) {
+      return `<details>\n<summary>${file}</summary>\n\n\`\`\`diff\n${message}\n\`\`\`\n\n</details>`;
+    }
+  })
+);
+
+const filteredResults = results.filter(Boolean);
+
+if (filteredResults.length) {
+  console.log('## `legacy-html` generator');
+  filteredResults.forEach(o => console.log(o));
+}
---
+++ scripts/compare-builds/utils.mjs
@@ -0,0 +1,4 @@
+import { fileURLToPath } from 'url';
+
+export const BASE = fileURLToPath(import.meta.resolve('../../base'));
+export const HEAD = fileURLToPath(import.meta.resolve('../../out'));
