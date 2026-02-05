072f5c0f210691e378b029191a86088b8c4b49c2
Claudio Wunder <cwunder@gnome.org>
2026-02-05 03:23:09 +0100

    chore: footer on search

--- src/generators/web/ui/components/SearchBox/index.jsx
+++ src/generators/web/ui/components/SearchBox/index.jsx
@@ -1,18 +1,52 @@
+import {
+  ArrowDownIcon,
+  ArrowTurnDownLeftIcon,
+  ArrowUpIcon,
+} from '@heroicons/react/24/solid';
 import SearchModal from '@node-core/ui-components/Common/Search/Modal';
 import SearchResults from '@node-core/ui-components/Common/Search/Results';
 import SearchHit from '@node-core/ui-components/Common/Search/Results/Hit';
 
+import styles from './index.module.css';
 import useOrama from '../../hooks/useOrama.mjs';
 
 const SearchBox = () => {
   const client = useOrama();
 
   return (
-    <SearchModal client={client} placeholder={'Start typing...'}>
-      <SearchResults
-        noResultsTitle={'No results found for'}
-        onHit={hit => <SearchHit document={hit.document} />}
-      />
+    <SearchModal client={client} placeholder="Start typing...">
+      <div className={styles.searchResultsContainer}>
+        <SearchResults
+          noResultsTitle="No results found for"
+          onHit={hit => <SearchHit document={hit.document} />}
+        />
+      </div>
+
+      <div className={styles.footer}>
+        <div className={styles.shortcutWrapper}>
+          <div className={styles.shortcutItem}>
+            <kbd className={styles.shortcutKey}>
+              <ArrowTurnDownLeftIcon />
+            </kbd>
+            <span className={styles.shortcutLabel}>to select</span>
+          </div>
+
+          <div className={styles.shortcutItem}>
+            <kbd className={styles.shortcutKey}>
+              <ArrowDownIcon />
+            </kbd>
+            <kbd className={styles.shortcutKey}>
+              <ArrowUpIcon />
+            </kbd>
+            <span className={styles.shortcutLabel}>to navigate</span>
+          </div>
+
+          <div className={styles.shortcutItem}>
+            <kbd className={styles.shortcutKey}>esc</kbd>
+            <span className={styles.shortcutLabel}>to close</span>
+          </div>
+        </div>
+      </div>
     </SearchModal>
   );
 };
