240d262f99c5782767f1517a8ddb7a5b7944c5e3
Aviv Keller <me@aviv.sh>
2026-03-27 17:03:22 -0400

    hotfix(mdn): type map URL fixes

--- src/generators/metadata/utils/__tests__/transformers.test.mjs
+++ src/generators/metadata/utils/__tests__/transformers.test.mjs
@@ -7,14 +7,14 @@ describe('transformTypeToReferenceLink', () => {
   it('should transform a JavaScript primitive type into a Markdown link', () => {
     strictEqual(
       transformTypeToReferenceLink('string'),
-      '[`<string>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type)'
+      '[`<string>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#string_type)'
     );
   });
 
   it('should transform a JavaScript global type into a Markdown link', () => {
     strictEqual(
       transformTypeToReferenceLink('Array'),
-      '[`<Array>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)'
+      '[`<Array>`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Array)'
     );
   });
 
@@ -30,35 +30,35 @@ describe('transformTypeToReferenceLink', () => {
   it('should transform a basic Generic type into a Markdown link', () => {
     strictEqual(
       transformTypeToReferenceLink('{Promise<string>}'),
-      '[`<Promise>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)&lt;[`<string>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type)&gt;'
+      '[`<Promise>`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Promise)&lt;[`<string>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#string_type)&gt;'
     );
   });
 
   it('should partially transform a Generic type if only one part is known', () => {
     strictEqual(
       transformTypeToReferenceLink('{CustomType<string>}', {}),
-      '`<CustomType>`&lt;[`<string>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type)&gt;'
+      '`<CustomType>`&lt;[`<string>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#string_type)&gt;'
     );
   });
 
   it('should transform a Generic type with an inner union like {Promise<string|boolean>}', () => {
     strictEqual(
       transformTypeToReferenceLink('{Promise<string|boolean>}', {}),
-      '[`<Promise>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)&lt;[`<string>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type) | [`<boolean>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#boolean_type)&gt;'
+      '[`<Promise>`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Promise)&lt;[`<string>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#string_type) | [`<boolean>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#boolean_type)&gt;'
     );
   });
 
   it('should transform multi-parameter generics like {Map<string, number>}', () => {
     strictEqual(
       transformTypeToReferenceLink('{Map<string, number>}', {}),
-      '[`<Map>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map)&lt;[`<string>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type), [`<number>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#number_type)&gt;'
+      '[`<Map>`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Map)&lt;[`<string>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#string_type), [`<number>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#number_type)&gt;'
     );
   });
 
   it('should handle outer unions with generics like {Promise<string|number> | boolean}', () => {
     strictEqual(
       transformTypeToReferenceLink('{Promise<string|number> | boolean}', {}),
-      '[`<Promise>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)&lt;[`<string>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#string_type) | [`<number>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#number_type)&gt; | [`<boolean>`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#boolean_type)'
+      '[`<Promise>`](https://developer.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/Promise)&lt;[`<string>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#string_type) | [`<number>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#number_type)&gt; | [`<boolean>`](https://developer.mozilla.org/docs/Web/JavaScript/Data_structures#boolean_type)'
     );
   });
 });
