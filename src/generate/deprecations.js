edd49992b4dcebc520d3405f815fc1ac387b144d
vanta.nox <200978133+Ap-0007@users.noreply.github.com>
2026-07-18 13:04:41 +0530

    fix: include deprecation ID in table of contents

---
+++ src/generate/deprecations.js
@@ -0,0 +1,16 @@
+const toc = [
+  {
+    type: 'heading',
+    depth: 2,
+    children: [
+      {
+        type: 'text',
+        value: 'Deprecations',
+      },
+      {
+        type: 'text',
+        value: ' (DEP-XXXX)',
+      },
+    ],
+  },
+];
