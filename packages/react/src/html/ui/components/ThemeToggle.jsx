1d9cb88505f86321ce81458d95c208130a9cc84b
Jonathan Amponsah <82057176+mgalore@users.noreply.github.com>
2026-08-10 18:04:39 +0000

    fix(react): keep system toggle icon

--- packages/react/src/html/ui/components/ThemeToggle.jsx
+++ packages/react/src/html/ui/components/ThemeToggle.jsx
@@ -1,7 +1,5 @@
-import { MoonIcon, SunIcon } from '@heroicons/react/24/outline';
 import ThemeToggle from '@node-core/ui-components/Common/ThemeToggle';
 
-import styles from './ThemeToggle.module.css';
 import { useTheme } from '../hooks/useTheme.mjs';
 import withIsland from '../islands/withIsland.jsx';
 
@@ -9,25 +7,10 @@ import withIsland from '../islands/withIsland.jsx';
  * Theme switcher.
  */
 const Toggle = () => {
-  const [themePreference, setThemePreference] = useTheme();
+  const [currentTheme, setThemePreference] = useTheme();
 
   return (
-    <span
-      className={styles.themeToggleWrapper}
-      data-theme-preference={themePreference}
-    >
-      <ThemeToggle
-        onChange={setThemePreference}
-        currentTheme={themePreference}
-      />
-
-      {themePreference === 'system' && (
-        <span className={styles.systemThemeIcon} aria-hidden="true">
-          <SunIcon data-theme-icon="light" height="20" />
-          <MoonIcon data-theme-icon="dark" height="20" />
-        </span>
-      )}
-    </span>
+    <ThemeToggle onChange={setThemePreference} currentTheme={currentTheme} />
   );
 };
 
